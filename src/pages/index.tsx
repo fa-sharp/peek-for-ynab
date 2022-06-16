f849b4a151c505f34e7aaa2c6a5757848565c164
fa-sharp <farshadkt@gmail.com>
2022-06-16 12:36:39 +0000

    Added next for website

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function IndexPage() {
+  return (
+    <main>
+      <h1>Welcome!</h1>
+    </main>
+  );
+}
+
+export default IndexPage;
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,10 @@
+function IndexPage() {
+  return (
+    <main>
+      <h1>Privacy Policy</h1>
+      <p>We will never knowningly sell or give away user data.</p>
+    </main>
+  );
+}
+
+export default IndexPage;
