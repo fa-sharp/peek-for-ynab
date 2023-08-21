129c17a77989c79860d9f0cf2ff3e59099127428
fa-sharp <farshadkt@gmail.com>
2023-08-20 22:17:40 -0400

    fleshed out account tx detail

--- src/components/AccountTxsView.tsx
+++ src/components/AccountTxsView.tsx
@@ -1,37 +1,62 @@
-import React from "react";
-import * as ynab from "ynab";
+import React, { useMemo } from "react";
 
 import { useYNABContext } from "~lib/context";
 
 import CurrencyView from "./CurrencyView";
+import TransactionView from "./TransactionView";
 
 type Props = {
   id: string;
 };
 
 const AccountTxsView = ({ id }: Props) => {
-  const { useGetAccountTxs, selectedBudgetData } = useYNABContext();
+  const { useGetAccountTxs, accountsData, selectedBudgetData } = useYNABContext();
 
+  const account = useMemo(
+    () => accountsData?.find((a) => a.id === id),
+    [accountsData, id]
+  );
   const { data: accountTxs } = useGetAccountTxs(id);
+
+  // Todo: Loading/error states
+  if (!account || !accountTxs || !selectedBudgetData) return null;
 
   return (
     <div>
-      <h1>Account transactions</h1>
-      {accountTxs?.map((tx) => {
-        const date = ynab.utils.convertFromISODateString(tx.date);
-        return (
-          <div key={tx.id} className="balance-display">
-            <div>{`${date.getUTCMonth() + 1}/${date.getUTCDate()}, ${
-              tx.payee_name
-            }`}</div>
-            <CurrencyView
-              milliUnits={tx.amount}
-              currencyFormat={selectedBudgetData?.currencyFormat}
-              colorsEnabled
-            />
-          </div>
-        );
-      })}
+      <h2 className="heading-big mb-small">{account.name}</h2>
+      <div className="balance-display">
+        Working Balance:
+        <CurrencyView
+          milliUnits={account.balance}
+          currencyFormat={selectedBudgetData.currencyFormat}
+          colorsEnabled
+        />
+      </div>
+      <div className="balance-display">
+        Cleared:
+        <CurrencyView
+          milliUnits={account.cleared_balance}
+          currencyFormat={selectedBudgetData.currencyFormat}
+          colorsEnabled
+        />
+      </div>
+      <div className="balance-display">
+        Uncleared:
+        <CurrencyView
+          milliUnits={account.uncleared_balance}
+          currencyFormat={selectedBudgetData.currencyFormat}
+          colorsEnabled
+        />
+      </div>
+      <h3 className="heading-medium mt-big">Transactions</h3>
+      {accountTxs.map((tx) => (
+        <TransactionView
+          key={tx.id}
+          tx={tx}
+          detailRight="category"
+          currencyFormat={selectedBudgetData.currencyFormat}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/components/TransactionView.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { ChevronDown, ChevronRight } from "tabler-icons-react";
+import * as ynab from "ynab";
+
+import CurrencyView from "./CurrencyView";
+import IconButton from "./IconButton";
+import TxStatusIcon from "./TxStatusIcon";
+
+export default function TransactionView({
+  tx,
+  detailLeft = "memo",
+  detailRight = "category",
+  currencyFormat
+}: {
+  tx: ynab.TransactionDetail | ynab.HybridTransaction;
+  detailLeft?: "memo";
+  detailRight?: "category" | "account";
+  currencyFormat?: ynab.CurrencyFormat;
+}) {
+  const [expanded, setExpanded] = useState(false);
+  const date = ynab.utils.convertFromISODateString(tx.date);
+
+  return (
+    <>
+      <div
+        className="balance-display cursor-pointer"
+        onClick={() => setExpanded((prev) => !prev)}>
+        <div className="flex-row">
+          <IconButton
+            icon={
+              expanded ? (
+                <ChevronDown color="var(--action)" size="0.9rem" />
+              ) : (
+                <ChevronRight color="var(--action)" size="0.9rem" />
+              )
+            }
+            label={expanded ? "Collapse" : "Expand"}
+          />
+          <div>{`${date.getUTCMonth() + 1}/${date.getUTCDate()}`}</div>
+          {tx.payee_name}
+        </div>
+        <div className="flex-row">
+          <CurrencyView
+            milliUnits={tx.amount}
+            currencyFormat={currencyFormat}
+            colorsEnabled
+          />
+          <TxStatusIcon status={tx.cleared} />
+        </div>
+      </div>
+      {expanded && (
+        <div
+          className="font-small"
+          style={{ display: "flex", justifyContent: "space-between", gap: "4px" }}>
+          {tx.memo && detailLeft === "memo" ? (
+            <div>
+              {tx.memo.slice(0, 40)}
+              {tx.memo.length > 40 ? "..." : ""}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <div
+            style={{
+              borderLeft: "solid 1px var(--border-light)",
+              paddingLeft: "4px"
+            }}>
+            {tx.transfer_account_id
+              ? "Transfer"
+              : detailRight === "category"
+              ? tx.category_name
+              : tx.account_name}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
