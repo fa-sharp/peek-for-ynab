682637706e06e2530bb83d5b521bf3fc5add3e88
fa-sharp <farshadkt@gmail.com>
2024-01-14 02:10:54 -0500

    start testing react elements

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {},
-  plugins: [tsConfigPaths()]
+  plugins: [tsConfigPaths(), react()]
 });
