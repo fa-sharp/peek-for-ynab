2bc80979580e2932e54a6e63d5d95c8213fbee85
fa-sharp <farshadkt@gmail.com>
2024-02-03 00:28:27 -0500

    try caching categories (but not accounts)

--- src/lib/context/index.tsx
+++ src/lib/context/index.tsx
@@ -10,7 +10,7 @@ import { StorageProvider, useStorageContext } from "./storageContext";
 import { YNABProvider, useYNABContext } from "./ynabContext";
 
 /** Queries that should be cached */
-const cachedQueryKeys = new Set(["budgets", "payees"]);
+const cachedQueryKeys = new Set(["budgets", "payees", "categoryGroups"]);
 
 /** React Query client, default settings */
 const queryClient = new QueryClient({
