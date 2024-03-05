51dc6a6bcb549b927c6c31f978bf4c1558dfeb51
fa-sharp <farshadkt@gmail.com>
2024-03-05 02:31:22 -0500

    merge fixes

--- src/components/TxStatusIcon.tsx
+++ src/components/TxStatusIcon.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { CircleC, Lock } from "tabler-icons-react";
-import { SaveTransactionClearedEnum } from "ynab";
+import { TransactionClearedStatus } from "ynab";
 
 type Props = {
-  status: SaveTransactionClearedEnum;
+  status: TransactionClearedStatus;
 };
 
 const TxStatusIcon = ({ status }: Props) =>
-  status === SaveTransactionClearedEnum.Cleared ? (
+  status === TransactionClearedStatus.Cleared ? (
     <CircleC
       fill="var(--currency-green)"
       stroke="var(--background)"
       aria-label="Cleared"
     />
-  ) : status === SaveTransactionClearedEnum.Reconciled ? (
+  ) : status === TransactionClearedStatus.Reconciled ? (
     <Lock stroke="var(--currency-green)" color="white" aria-label="Reconciled" />
   ) : (
     <CircleC aria-label="Uncleared" stroke="gray" />
