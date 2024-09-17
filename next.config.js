f124b7c153484d14218cc5e0275f2c5c42b5ff97
fa-sharp <farshadkt@gmail.com>
2024-09-17 17:28:35 -0400

    update cors config

--- next.config.js
+++ next.config.js
@@ -7,27 +7,6 @@ const config = {
   experimental: {
     serverComponentsExternalPackages: ["pino"]
   },
-  headers: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.EXTENSION_ORIGIN || "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
   redirects: async () => [
     {
       source: '/:path*',
