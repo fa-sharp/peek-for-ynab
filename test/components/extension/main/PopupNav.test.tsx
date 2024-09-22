4a17c3e4e46d53ca9948cc3d6c3da5f5772b934d
fa-sharp <farshadkt@gmail.com>
2024-09-22 04:07:38 -0400

    fix import

--- test/components/extension/main/PopupNav.test.tsx
+++ test/components/extension/main/PopupNav.test.tsx
@@ -1,10 +1,9 @@
 import { render, renderHook, screen, waitFor } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
-import { afterEach } from "node:test";
 import { validToken } from "test/mock/userData";
 import { createTestAppWrapper } from "test/mock/wrapper";
 import { budgets } from "test/mock/ynabApiData";
-import { beforeEach, expect, test } from "vitest";
+import { afterEach, beforeEach, expect, test } from "vitest";
 import "vitest-dom/extend-expect";
 
 import { PopupNav } from "~components";
