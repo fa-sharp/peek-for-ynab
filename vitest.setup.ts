53ea76e3ac372930d582357cf5453f55eb10ba88
fa-sharp <farshadkt@gmail.com>
2024-09-23 04:06:19 -0400

    fix test

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -5,7 +5,11 @@ import { afterAll, afterEach, beforeAll, vi } from "vitest";
 //@ts-expect-error hack to make some jest mocks work with vitest
 globalThis.jest = vi;
 
-beforeAll(() => mockServer.listen());
+beforeAll(() => {
+  mockServer.listen();
+  // mock needed for this extension API, used to detect if we're currently in a popup
+  chrome.extension.getViews = vi.fn().mockImplementation(() => ["window"]);
+});
 afterEach(() => {
   mockServer.resetHandlers();
   chrome.storage.local.clear();
