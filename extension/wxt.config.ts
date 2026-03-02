14054c108b88585de5d2ee6c6dc63723c6a8ce47
fa-sharp <farshadkt@gmail.com>
2026-03-02 06:12:58 -0500

    clean up, astro build check

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   imports: false, // disable auto-imports
   targetBrowsers: ["chrome", "firefox"],
   vite: () => ({
-    envPrefix: "PUBLIC_"
+    envPrefix: "PUBLIC_",
   }),
   manifest: {
     name: "Peek for YNAB",
@@ -16,31 +16,31 @@ export default defineConfig({
     optional_permissions: ["scripting", "activeTab", "notifications"],
     host_permissions: ["https://api.ynab.com/*"],
     omnibox: {
-      keyword: "peek"
+      keyword: "peek",
     },
     commands: {
       _execute_action: {
         suggested_key: {
-          default: "Alt+Shift+Y"
+          default: "Alt+Shift+Y",
         },
-        description: "Open the extension popup"
-      }
+        description: "Open the extension popup",
+      },
     },
     browser_specific_settings: {
       gecko: {
         id: "{e734411e-6aae-4590-ab10-65e7a226b311}",
-        strict_min_version: "109.0"
-      }
-    }
+        strict_min_version: "109.0",
+      },
+    },
   },
   alias: {
     "~components": "src/components",
-    "~lib": "src/lib"
+    "~lib": "src/lib",
   },
   autoIcons: { baseIconPath: "assets/icon512.png" },
   webExt: {
     chromiumArgs: [`--user-data-dir=${resolve(".wxt/chrome-data")}`],
-    keepProfileChanges: true
+    keepProfileChanges: true,
   },
-  modules: ["@wxt-dev/auto-icons", "@wxt-dev/module-react"]
+  modules: ["@wxt-dev/auto-icons", "@wxt-dev/module-react"],
 });
