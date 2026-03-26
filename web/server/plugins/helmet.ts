2804b3603f95ad6d877a72cee16bf723bbde189b
fa-sharp <farshadkt@gmail.com>
2026-03-26 12:16:50 -0400

    web: use browser-native view transitions with strict CSP generated by Astro

--- web/server/plugins/helmet.ts
+++ web/server/plugins/helmet.ts
@@ -4,17 +4,6 @@ import fastifyPlugin from "fastify-plugin";
 /** Add security headers to all requests */
 export default fastifyPlugin(async (app) => {
   app.register(fastifyHelmet, {
-    contentSecurityPolicy: {
-      useDefaults: false,
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'", "https://fonts.googleapis.com"],
-        imgSrc: ["'self'"],
-        fontSrc: ["'self'", "https://fonts.gstatic.com"],
-        objectSrc: ["'none'"],
-        upgradeInsecureRequests: [],
-      },
-    },
+    contentSecurityPolicy: false, // CSP added by Astro to HTML pages
   });
 });
