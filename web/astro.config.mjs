6f87bcd3490300b3831c59bcd6b1a9b94fb579f1
fa-sharp <farshadkt@gmail.com>
2026-04-03 13:04:48 -0400

    web: remove unused env vars, remove unused deps

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,23 +1,12 @@
 // @ts-check
 
 import mdx from "@astrojs/mdx";
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), mdx()],
-  env: {
-    schema: {
-      YNAB_CLIENT_ID: envField.string({ context: "server", access: "secret" }),
-      YNAB_SECRET: envField.string({ context: "server", access: "secret" }),
-      YNAB_TOKEN_URL: envField.string({
-        context: "server",
-        access: "public",
-        default: "https://app.ynab.com/oauth/token",
-      }),
-    },
-  },
   image: {
     responsiveStyles: true,
   },
