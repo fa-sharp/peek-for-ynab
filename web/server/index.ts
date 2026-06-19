feaa67c6ac7ec38ebb4f7f0e3292381569f1e93e
fa-sharp <farshadkt@gmail.com>
2026-06-19 02:09:51 -0400

    remove old server

--- web/server/index.ts
+++
@@ -1,5 +0,0 @@
-import { createServer } from "./server.ts";
-
-// Start the server
-const app = await createServer();
-await app.listen({ host: process.env.HOST, port: +(process.env.PORT || 4321) });
--- web/server/plugins/helmet.ts
+++
@@ -1,9 +0,0 @@
-import fastifyHelmet from "@fastify/helmet";
-import fastifyPlugin from "fastify-plugin";
-
-/** Add security headers to all requests */
-export default fastifyPlugin(async (app) => {
-  app.register(fastifyHelmet, {
-    contentSecurityPolicy: false, // CSP added by Astro to HTML pages
-  });
-});
